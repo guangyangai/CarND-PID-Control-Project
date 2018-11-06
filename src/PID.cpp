4e3e3f7c393c4557f5255deb2dddce2add7dc503
guangyangai <guang.k.yang@gmail.com>
2018-11-06 04:38:50 +0000

    add twiddle function

--- src/PID.cpp
+++ src/PID.cpp
@@ -31,3 +31,33 @@ double PID::TotalError() {
   return this->i_error;
 }
 
+void PID::Twiddle(double cte, double speed, double angle, int steps, double tol){
+  p = {Kp, Kd, Ki};
+  dp = {0.1*Kp, 0.1*Kd, 0.1*Ki};
+  //run the simulator forward for N steps 
+  best_err = run(robot, p);
+  while (sum(dp) > tol) {
+    for(unsigned i=0; i<p.size(); i++){
+      p[i] += dp[i];
+      //run the simulator forward for N steps
+      new_err = run(robot, p);
+      if(new_err < best_err){
+        best_error = new_err;
+        dp[i]*=1.1;
+      }else{
+        //tru minus dp
+        p[i] -= 2*dp[i];
+        //forward the simulator N times 
+        new_err = run(robot, p);
+        if(new_err < best_err){
+          best_error = new_err;
+          dp[i]*=1.1;
+        }else{
+          p[i] += dp[i];
+          dp[i] *= 0.9;
+        }
+      }//end of previous else
+    }//end of for
+  }//end of while
+}
+
